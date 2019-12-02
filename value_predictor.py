e17926f1e4cc70ad39d539b3ad7f57cbb345accf
adrtee <58418968+adrtee@users.noreply.github.com>
2019-12-02 19:19:18 +0800

    Update value_predictor.py

--- value_predictor.py
+++ value_predictor.py
@@ -51,12 +51,10 @@ def inputNum():
             else:
                 print("Please enter integer greater or equal to the previous entry")
 
-#this is the function of calculating mean      
+#this is the function of calculating average of the two entries in the center of the list      
 def findCenter(numbers, l):  
-    sum = 0
-    for x in range( 0, l): 
-        sum += numbers[x]       
-    return float(sum/l)
+    sum = numbers[int(l/2)-1] + numbers[int(l/2)]
+    return float(sum/2)
 
 #main code
 print("Enter at least 6 increasing integers and the total entries must be an even number")
