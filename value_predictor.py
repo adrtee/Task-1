e6abe7ef2cc0d9e1b862b40ba03a927b0d11573e
adrtee <58418968+adrtee@users.noreply.github.com>
2019-12-02 19:04:00 +0800

    value_predictor.py
    Task 1
    Adrian Tee Lian Guang

---
+++ value_predictor.py
@@ -0,0 +1,69 @@
+#define list
+numbers = [] 
+
+#define functions
+#this is the function to obtain input
+def inputNum():
+    count = 0
+
+    #For the first six entries
+    while count < 6:
+        #check whether the entry is integer
+        try:
+            entry=int(input())
+            integer = 1
+                      
+        except:
+            integer = 0
+            print ("please enter integer only")
+
+        #check whether the entry is greater or equal to the previous one    
+        if integer == 1:
+            if count == 0:
+                numbers.append(entry)
+                count = count + 1
+            elif entry >= numbers[count-1] and count > 0:
+                numbers.append(entry)
+                count = count + 1
+            else:
+                print("Please enter integer greater or equal to the previous entry")
+
+    #Additional entries
+    print ("Continue entering increasing integers or enter any alphabet to calculate")
+    while count >= 6:
+        #check whether the entry is integer
+        try: 
+            entry=int(input())
+            integer = 1
+            
+        except:
+            integer = 0
+            if (count % 2) == 0:
+                return numbers   
+            else:
+                print ("The total entry must be a even number, the number of entries is ", count,". Please enter at least 1 more integer") 
+
+        #check whether the entry is greater or equal to the previous one
+        if integer == 1:
+            if entry >= numbers[count-1]:
+                numbers.append(entry)
+                count = count + 1
+            else:
+                print("Please enter integer greater or equal to the previous entry")
+
+#this is the function of calculating mean      
+def findCenter(numbers, l):  
+    sum = 0
+    for x in range( 0, l): 
+        sum += numbers[x]       
+    return float(sum/l)
+
+#main code
+print("Enter at least 6 increasing integers and the total entries must be an even number")
+numbers = inputNum()
+l = len(numbers)
+print(numbers)
+print("Center =", findCenter(numbers, l))
+
+
+
